9f2dcec575002a6715ac940f038fe7177fc7a13a
Karim <ksenousi@gmail.com>
2020-04-16 22:41:07 +0100

    added entry model

---
+++ models/entry.model.js
@@ -0,0 +1,19 @@
+const EntryModel = {
+  name: {
+    isString: true,
+    isLength: {
+      errorMessage: "name should not be empty",
+      options: { min: 1 },
+    },
+  },
+  word: {
+    isString: true,
+    isLength: {
+      errorMessage: "word should not be empty",
+      options: { min: 1 },
+    },
+  },
+};
+
+// since this model should never be modified at runtime it's better to freeze it
+module.exports = Object.freeze(EntryModel);
